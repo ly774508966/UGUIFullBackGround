293d16560bacefb5279d034a00b7ae368a851728
zzlple <zzlple@192.168.3.130>
2016-07-08 23:11:04 +0800

    给出一张图始终填充屏幕的方案

---
+++ Assets/Scripts/AutoScaleManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class AutoScaleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Image image = GetComponent<Image> ();
+		image.preserveAspect = true;
+		RectTransform rt = (RectTransform)transform;
+		rt.anchorMin = Vector2.zero;
+		rt.anchorMax = Vector2.one;
+		rt.offsetMin = Vector2.zero;
+		rt.offsetMax = Vector2.zero;
+		float screenWidth = (float)Screen.width;
+		float screenHeight = (float)Screen.height;
+		float imageWidth = (float)image.preferredWidth;
+		float imageHeight = (float)image.preferredHeight;
+		Debug.Log (imageWidth + ":" +imageHeight);
+		float screenRate =screenWidth/screenHeight;
+		float imageRate = imageWidth/imageHeight;
+
+	
+
+		if (screenRate > imageRate) {
+
+			Debug.Log ("高度填充");
+			rt.localScale = rt.localScale *
+				screenWidth / (imageRate * screenHeight);
+
+		} else {
+			Debug.Log("宽度填充");
+
+
+			rt.localScale = rt.localScale *
+				(screenHeight/(screenWidth / imageRate) );
+
+
+		}
+
+
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
